9de6a9ee5776022d87720718e181828f06716fba
Thauan Mendes <thauan.mendes@appratico.com>
2021-08-05 18:36:01 -0300

    Test - Chamando metodo getmult

--- apibanco/calculo.go
+++ apibanco/calculo.go
@@ -1,8 +1,22 @@
 package apibanco
 
-var code []ApiBanco
+import (
+	"context"
+	"site/utils/log"
+
+	"cloud.google.com/go/datastore"
+)
+
+var c context.Context
+var keys []*datastore.Key
 
 func CalculaCode() int64 {
+	code, err := GetMultAPIBanco(c, keys)
+	if err != nil {
+		log.Warningf(c, "Erro ao api banco")
+		return 0
+	}
+
 	var soma int64
 	soma = 0
 	for _, v := range code {
